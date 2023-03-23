8771f7b93dfdda01a05ecd8adf2b9a01e4809c38
RuilaiZhang <ray.zhangruilai@gmail.com>
2023-03-23 22:02:48 +1100

    update tool

--- actionTest.sh
+++ actionTest.sh
@@ -5,14 +5,16 @@ else
     TESTCHAIN="goerli"
 fi
 
-# If there are changes in proxy.sol or verify.sol, need to redeploy
+# If there are changes in proxy.sol or verify.sol, need to redeploy file2
 # npx truffle migrate --f 2 --to 2 --network ${TESTCHAIN}
+# If there are more changes other than proxy.sol and verify.sol, need to re-deploy all
+# npx truffle migrate --network ${TESTCHAIN}
 
-# node dist/clients/config-contracts-info.js
-# node dist/tests/action_test.js addpool ${TESTCHAIN}
-# node dist/tests/action_test.js deposit ${TESTCHAIN}
-# node dist/tests/action_test.js retrive ${TESTCHAIN}
-# node dist/tests/action_test.js setkey ${TESTCHAIN}
-# node dist/tests/action_test.js supply ${TESTCHAIN}
-# node dist/tests/action_test.js swap ${TESTCHAIN}
-# node dist/tests/action_test.js withdraw ${TESTCHAIN}
+node dist/clients/config-contracts-info.js
+node dist/tests/action_test.js addpool ${TESTCHAIN}
+node dist/tests/action_test.js deposit ${TESTCHAIN}
+node dist/tests/action_test.js retrive ${TESTCHAIN}
+node dist/tests/action_test.js setkey ${TESTCHAIN}
+node dist/tests/action_test.js supply ${TESTCHAIN}
+node dist/tests/action_test.js swap ${TESTCHAIN}
+node dist/tests/action_test.js withdraw ${TESTCHAIN}
