b80448bcbe86dec2002af1ac8a6f9fff8031307a
Glen <glen@zkcross.org>
2024-11-04 10:04:48 +0800

    add topup test
    Signed-off-by: Glen <glen@zkcross.org>

---
+++ scripts/topup.ts
@@ -0,0 +1,20 @@
+const { ethers } = require("hardhat");
+import * as constants from "./const.ts";
+
+async function main() {
+	const proxy = await ethers.getContractAt("Proxy", constants.proxyAddress);
+
+	const token = await ethers.getContractAt("Token", constants.tokenAddress);
+
+        var tx = await token.approve(proxy.address, ethers.utils.parseUnits("1000","ether")); //for deposit test
+	await tx.wait();
+
+	const result = await proxy.topup(0, 10);
+	await result.wait();
+	console.log("result:", result)
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
