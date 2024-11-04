4805668adef49d9596e25ffc68472bcc198f575a
Glen <glen@zkcross.org>
2024-11-04 18:13:49 +0800

    update token name
    Signed-off-by: Glen <glen@zkcross.org>

--- contracts/assist/Token.sol
+++ contracts/assist/Token.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 //import "@openzeppelin/contracts/utils/Counters.sol";
 contract Token is ERC20{
-  constructor() ERC20("Titan Token", "Ti") {
+  constructor() ERC20("Memedisco Token", "Di") {
     _mint(msg.sender, 2e9 * 1e18); //2 billion
   }
   function getBalance() public view returns (uint256) {
